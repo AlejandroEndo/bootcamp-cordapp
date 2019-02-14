de77c9b3c4c916da82422cc769b432a49c91cf8f
Alejandro Endo <alejoendo@hotmail.com>
2019-02-14 15:45:24 -0500

    Initial commit

---
+++ src/main/java/java_bootcamp/HouseState.java
@@ -0,0 +1,17 @@
+package java_bootcamp;
+
+import net.corda.core.contracts.ContractState;
+import net.corda.core.identity.AbstractParty;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class HouseState implements ContractState {
+
+
+    @NotNull
+    @Override
+    public List<AbstractParty> getParticipants() {
+        return null;
+    }
+}
