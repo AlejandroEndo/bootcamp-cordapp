9df36aaff44eacf177e6ad9417a0e9cdce021c04
Alejandro Endo <alejoendo@hotmail.com>
2019-02-17 12:47:33 -0500

    House/Container States

--- src/main/java/java_bootcamp/HouseState.java
+++ src/main/java/java_bootcamp/HouseState.java
@@ -1,17 +1,39 @@
 package java_bootcamp;
 
+import com.google.common.collect.ImmutableList;
 import net.corda.core.contracts.ContractState;
 import net.corda.core.identity.AbstractParty;
+import net.corda.core.identity.Party;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
 public class HouseState implements ContractState {
 
+    private String address;
+    private Party owner;
+
+    public HouseState(String address, Party owner){
+        this.address = address;
+        this.owner = owner;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public Party getOwner() {
+        return owner;
+    }
+
+    public static void main(String[] args){
+        Party joel = null;
+        HouseState state = new HouseState("Calle falsa 123", joel);
+    }
 
     @NotNull
     @Override
     public List<AbstractParty> getParticipants() {
-        return null;
+        return ImmutableList.of(owner);
     }
 }
