053959d32af8ce412e81baef95679e1a6b84917a
Alejandro Endo <alejoendo@hotmail.com>
2019-02-17 13:04:49 -0500

    Test Token States

--- src/main/java/java_bootcamp/TokenState.java
+++ src/main/java/java_bootcamp/TokenState.java
@@ -4,11 +4,44 @@ import com.google.common.collect.ImmutableList;
 import net.corda.core.contracts.ContractState;
 import net.corda.core.identity.AbstractParty;
 import net.corda.core.identity.Party;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /* Our state, defining a shared fact on the ledger.
  * See src/main/java/examples/ArtState.java for an example. */
-public class TokenState {
+public class TokenState implements ContractState {
 
+    private int amount;
+
+    private Party owner;
+    private Party issuer;
+
+    public TokenState(Party issuer, Party owner, int amount) {
+        this.issuer = issuer;
+        this.owner = owner;
+        this.amount = amount;
+    }
+
+    public Party getOwner() {
+        return owner;
+    }
+
+    public Party getIssuer() {
+        return issuer;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    @NotNull
+    @Override
+    public List<AbstractParty> getParticipants() {
+        List<AbstractParty> participants = new ArrayList<>();
+        participants.add(issuer);
+        participants.add(owner);
+        return participants;
+    }
 }
